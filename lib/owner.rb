e2c34bb5c23aedbab6907ba407611fab4663e8f3
Jason  Montes <jasone.montes@gmail.com>
2019-05-23 15:29:41 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,14 +1,6 @@
 class Owner
   
-  attr_reader :species
-attr_accessor :name, :pets
-@@all = []
-
-def initialize(species)
-  @species = species
-  @@all << self
-  @pets = {:fishes => [], :dogs => [], :cats => []}
-end
+ 
 
   
   
